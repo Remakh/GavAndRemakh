a9cac34fdc4937e12c014b8ce6b5711319b98b14
Jiawen Huang <jiawenhuang1999@gmail.com>
2021-02-23 23:27:27 +0000

    Graphql Start

---
+++ backend/authentication/src/index.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers/user";
+const main = async () => {
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(8888);
+};
+
+main();
