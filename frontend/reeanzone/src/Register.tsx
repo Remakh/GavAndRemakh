46658a1b48bd0418bd1e0e6ecea212ef6309f69a
Jiawen Huang <jiawenhuang1999@gmail.com>
2021-03-01 00:04:15 +0000

    Styling

--- frontend/reeanzone/src/Register.tsx
+++ frontend/reeanzone/src/Register.tsx
@@ -1,4 +1,4 @@
-import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import React from "react";
 import {
@@ -9,7 +9,7 @@ import {
 const Register = () => {
   const [register] = useRegisterMutation();
   return (
-    <div>
+    <Box width={800} mx="auto">
       <Formik
         initialValues={{ username: "", password: "", email: "" }}
         onSubmit={(values) => {
@@ -45,7 +45,7 @@ const Register = () => {
           </Form>
         )}
       </Formik>
-    </div>
+    </Box>
   );
 };
 
